637b59c1073b1965bd9b9b5a772fd050a4fadc85
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-14 10:10:24 -0600

    successfully connection to API, JSON response with only Gryffindor members

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,10 @@
 class SearchController < ApplicationController
   def index
-
+    key = ENV['potter_api_key']
+    house = params[:house]
+    url = "https://www.potterapi.com/v1/characters?key=#{key}"
+    conn = Faraday.get(url, house: "#{house}")
+    response = JSON.parse(conn.body)
+    binding.pry
   end
 end
