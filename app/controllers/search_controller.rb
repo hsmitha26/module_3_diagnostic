dd59f5528ce6a8261b3ae06435291732d5031e1a
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-14 10:35:31 -0600

    removes variable key since ENV['potter_api_key'] is already accessible

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,7 @@
 class SearchController < ApplicationController
   def index
-    key = ENV['potter_api_key']
     @house = params[:house]
-    url = "https://www.potterapi.com/v1/characters?key=#{key}"
+    url = "https://www.potterapi.com/v1/characters?key=#{ENV['potter_api_key']}"
     conn = Faraday.get(url, house: "#{@house}", "orderOfThePhoenix": true)
     @response = JSON.parse(conn.body)
   end
