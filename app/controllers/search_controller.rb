add6b562b1f6c3fb4a2621e1ed7b5370fcd55c60
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-14 10:30:55 -0600

    formats the data to include only members who are members of the Order of the Phoenix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,9 @@
 class SearchController < ApplicationController
   def index
     key = ENV['potter_api_key']
-    house = params[:house]
+    @house = params[:house]
     url = "https://www.potterapi.com/v1/characters?key=#{key}"
-    conn = Faraday.get(url, house: "#{house}")
-    response = JSON.parse(conn.body)
-    binding.pry
+    conn = Faraday.get(url, house: "#{@house}", "orderOfThePhoenix": true)
+    @response = JSON.parse(conn.body)
   end
 end
