28accef204183a498477a52190e01869a367aafe
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-14 10:32:01 -0600

    test passing for un-refactored controller

--- spec/features/user_can_see_order_members_spec.rb
+++ spec/features/user_can_see_order_members_spec.rb
@@ -6,16 +6,19 @@ describe "As a user, when I visit '/'; select 'Gryffindor' from select field & '
 
     select 'Gryffindor', from: :house
     click_on 'Search For Members'
-save_and_open_page
+    expect(current_path).to eq('/search')
+
+    expect(page).to have_content("21 for Gryffindor")
+
+    within(first(".members")) do
+      expect(page).to have_content("")
+      expect(page).to have_content("")
+      expect(page).to have_content("")
+      expect(page).to have_content("")
+    end
   end
 end
-# As a user,
-# When I visit "/"
-# And I Select "Griffindor" from the select field
-# (Note: Use the existing select field)
-# And I click "Search For Members"
-# Then I should be on page "/search"
-# Then I should see a total of the number of members for that house. (18 for Griffindor)
+
 # Then I should see a list of 18 members
 #
 # And for each of the members I should see:
