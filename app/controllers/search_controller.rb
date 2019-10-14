0771755d60f3c9fb102808ad42c1829df7d9c7ee
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-14 10:51:42 -0600

    using MemberFacade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def index
-    @house = params[:house]
+    @house = MemberFacade.new(params[:house]).member_house
+
     url = "https://www.potterapi.com/v1/characters?key=#{ENV['potter_api_key']}"
     conn = Faraday.get(url, house: "#{@house}", "orderOfThePhoenix": true)
     @response = JSON.parse(conn.body)
